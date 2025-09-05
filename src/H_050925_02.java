8407e99159f2bf9179e48fff2acc18cbf665be44
Jakub Lukšík <jakub.luksik@infis.cz>
2025-09-05 11:29:06 +0200

    commit

--- src/H_050925_02.java
+++ src/H_050925_02.java
@@ -1,5 +1,10 @@
+import fileworks.DataExport;
+
 public class H_050925_02 {
     public static void main(String[] args) {
+        DataExport dataExport = new DataExport("output.txt");
 
+        dataExport.writeLine("ahoj, tohle je první line output souboru hahaha");
+        dataExport.finishExport();
     }
 }
