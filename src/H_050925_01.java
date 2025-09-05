70e7aa5020c473c0e825532578cc795d5b693f1d
Jakub Lukšík <jakub.luksik@infis.cz>
2025-09-05 11:25:30 +0200

    commit

---
+++ src/H_050925_01.java
@@ -0,0 +1,26 @@
+import fileworks.DataImport;
+
+import java.util.Scanner;
+
+public class H_050925_01 {
+    public static void main(String[] args) {
+        DataImport dataImport = new DataImport("randomtext.txt");
+
+        dataImport.printFile();
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line);
+
+        line = dataImport.readLine();
+        System.out.println(line);
+
+        line = dataImport.readLine();
+        System.out.println(line);
+
+        while(dataImport.hasNext()){
+            line = dataImport.readLine();
+            System.out.println(line);
+        }
+    }
+}
---
+++ src/H_050925_02.java
@@ -0,0 +1,5 @@
+public class H_050925_02 {
+    public static void main(String[] args) {
+
+    }
+}
