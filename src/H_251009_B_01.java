0f02f8003274799548f4a8fbff5aa9de3d7ee586
Jakub Lukšík <jakub.luksik@infis.cz>
2025-09-12 10:53:16 +0200

    commit

--- src/H_251009_B_01.java
+++ src/H_251009_B_01.java
@@ -1,27 +1,63 @@
 import fileworks.DataImport;
 
+import java.util.ArrayList;
+
 public class H_251009_B_01 {
     public static void main(String[] args) {
+        //nacti soubor
         DataImport di = new DataImport("countries.txt");
-        di.printFile();
-        System.out.println("1. země");
-        String countryData = di.readLine();
-        String[] dataPieces = countryData.split(";");
-        Country firstcountry = new Country(
+        //di.printFile();
+        System.out.println("prvni zeme:");
+        //String countryData = di.readLine();
+
+        //Country firstCountry = parseLine(countryData);
+        ArrayList<Country> countries = new ArrayList<>();
+        int i = 0;
+        while(di.hasNext()){
+            countries.add(parseLine(di.readLine()));
+            System.out.println(countries.get(i));
+            i++;
+        }
+
+//        String[] dataPieces = countryData.split(";");
+//        Country firstCountry = new Country(
+//                dataPieces[0],
+//                dataPieces[1],
+//                Long.parseLong(dataPieces[2]),
+//                Double.parseDouble(dataPieces[3])
+//        );
+
+        System.out.println(firstCountry.getName());
+        System.out.println(firstCountry.getAge());
+        System.out.println(firstCountry);
+
+
+
+        di.finishImport();
+    }
+
+    static Country parseLine(String line){
+        String[] dataPieces = line.split(";");
+        Country country = new Country(
                 dataPieces[0],
                 dataPieces[1],
                 Long.parseLong(dataPieces[2]),
                 Double.parseDouble(dataPieces[3])
         );
-        di.finishImport();
+        return country;
     }
 }
+
 class Country {
     public String name, continent;
     public long something;
     public double age;
 
-    public Country(String dataPiece, String dataPiece1, long l, double v) {
+    public Country(String name, String continent, long something, double age) {
+        this.name = name;
+        this.continent = continent;
+        this.something = something;
+        this.age = age;
     }
 
     public String getName() {
@@ -55,4 +91,11 @@ class Country {
     public void setAge(double age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return String.format("nazev: %s | kontinent: %s | populace: %,d | doziti: %.2f", name, continent, something, age);
+    }
 }
+
+
