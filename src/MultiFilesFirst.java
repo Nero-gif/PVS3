74f5493283594953c79dbb566910a2100978ac09
Jakub Lukšík <jakub.luksik@infis.cz>
2026-01-16 10:26:25 +0100

    commit

--- src/MultiFilesFirst.java
+++ src/MultiFilesFirst.java
@@ -1,9 +1,59 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class MultiFilesFirst {
     public static void main(String[] args) {
-        String path = ".\\testdata\\hello
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String pathIN = ".\\testdata\\znamky.txt";
+        String pathOUT = ".\\testdata\\znamkyPrumner.txt";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(pathIN));
+
+            FileWriter fw = new FileWriter(pathOUT);
+
+            String line;
+
+            double bestAverage = 10;
+            String bestName = "";
+
+            double worstAverage = 0;
+            String worstName = "";
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+
+
+                String[] marks = line.split(";");
+                int soucet = 0;
+                for (int i = 1; i < marks.length; i++) {
+                    soucet += Integer.parseInt(marks[i]);
+                }
+                double average;
+                average = (double) soucet / (marks.length - 1);
+                String write = marks[0] + ";" + average;
+                System.out.println(write);
+                fw.write(write + '\n');
+
+                if (bestAverage > average){
+                    bestAverage = average;
+                    bestName = marks[0];
+                }
+
+                if (worstAverage < average){
+                    worstAverage = average;
+                    worstName = marks[0];
+                }
+            }
+            String best = bestName + ";" +  bestAverage;
+            System.out.println("Nejlepší je " + best);
+
+            String worst = worstName + ";" +  worstAverage;
+            System.out.println("Nejhorší je " + worst);
+
+            br.close();
+            fw.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
